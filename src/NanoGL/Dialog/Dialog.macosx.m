0836d95ead0faf0b62280044fff04bb38b8314da
Sup3rc4l1fr4g1l1571c3xp14l1d0c10u5 <fefnir_@hotmail.com>
2016-11-29 22:45:32 +0900

    ダイアログボックス命令のMacOSで動作を確認。 ただし、ExampleのDialogのマウス押下検知の関係上、MacOSではファイル保存ダイアログを閉じてもすぐ開く不具合が発生してしまう。

--- src/NanoGL/Dialog/Dialog.macosx.m
+++ src/NanoGL/Dialog/Dialog.macosx.m
@@ -10,11 +10,11 @@ string_t OpenFileDialog(const char *title, const char *filter) {
 
         char *filter_copy = strdup(filter);
         char *capture = NULL;
-        for (char *p != filter_copy; *p != '\0'; p++) {
+        for (char *p = filter_copy; *p != '\0'; p++) {
             switch (*p) {
                 case '|' : 
                     *p = '\0'; 
-                    capture = p+1
+                    capture = p+1;
                     break;
                 case ';' : 
                     *p = '\0'; 
@@ -42,7 +42,8 @@ string_t OpenFileDialog(const char *title, const char *filter) {
                         capture = NULL;
                     }
                     break;
-                default: break;
+                default:
+                    break;
             }
         }
         if (capture != NULL) {
@@ -52,13 +53,19 @@ string_t OpenFileDialog(const char *title, const char *filter) {
         free(filter_copy);
         NSOpenPanel* openDlg = [NSOpenPanel openPanel];
         [openDlg setDirectoryURL:[NSURL fileURLWithPath: NSHomeDirectory()]];
-        [openDlg setAllowedFileTypes:naFileTypes]
+        [openDlg setAllowedFileTypes:naFileTypes];
         //[openDlg setPrompt: "Open"];
         [openDlg setMessage: nsTitle];
         NSInteger ret = [openDlg runModal];
         if (ret > 0) {
             NSString *path = [[openDlg URL] path];
-            return String.Create([path cStringUsingEncoding:NSUTF8StringEncoding]);
+            string_t result = String.Create([path cStringUsingEncoding:NSUTF8StringEncoding]);
+            
+            char *last_lf = strrchr(result.c_str,'\n');
+            if (last_lf != NULL && last_lf[1] == '\0') {
+                last_lf[0] = '\0';
+            }
+            return result;
         } else {
             return String.Create(NULL);
         }
@@ -69,20 +76,68 @@ string_t OpenFileDialogUTF8(const char *title, const char *filter) {
     return OpenFileDialog(title, filter);
 }
 
-string_t SaveFileDialog(const char *prompt, const char *message) {
+string_t SaveFileDialog(const char *title, const char *filter) {
     @autoreleasepool {
-        NSString* nsPrompt = (prompt == NULL) ? nil : [NSString stringWithUTF8String:prompt];
-        NSString* nsMessage = (message == NULL) ? nil : [NSString stringWithUTF8String:message];
+        NSString* nsTitle = (title == NULL) ? nil : [NSString stringWithUTF8String:title];
+        NSString* nsFilter = (filter == NULL) ? nil : [NSString stringWithUTF8String:filter];
+        NSMutableArray* naFileTypes = [@[] mutableCopy];
+        
+        char *filter_copy = strdup(filter);
+        char *capture = NULL;
+        for (char *p = filter_copy; *p != '\0'; p++) {
+            switch (*p) {
+                case '|' :
+                    *p = '\0';
+                    capture = p+1;
+                    break;
+                case ';' :
+                    *p = '\0';
+                    if (capture != NULL) {
+                        while (*capture != '.' && *capture != '\0') {
+                            capture++;
+                        }
+                        if (*capture == '.') {
+                            capture++;
+                            [naFileTypes addObject: [NSString stringWithUTF8String: capture]];
+                        }
+                        capture = p+1;
+                    }
+                    break;
+                case '\n' :
+                    *p = '\0';
+                    if (capture != NULL) {
+                        while (*capture != '.' && *capture != '\0') {
+                            capture++;
+                        }
+                        if (*capture == '.') {
+                            capture++;
+                            [naFileTypes addObject: [NSString stringWithUTF8String: capture]];
+                        }
+                        capture = NULL;
+                    }
+                    break;
+                default: break;
+            }
+        }
+        if (capture != NULL) {
+            [naFileTypes addObject: [NSString stringWithUTF8String: capture]];
+            capture = NULL;
+        }
+        free(filter_copy);
         NSSavePanel* openDlg = [NSSavePanel savePanel];
         [openDlg setDirectoryURL:[NSURL fileURLWithPath: NSHomeDirectory()]];
-        [openDlg setCanCreateDirectories:YES];
-        //[openDlg setPrompt: "Save"];
+        [openDlg setAllowedFileTypes:naFileTypes];
         [openDlg setMessage: nsTitle];
-        [openDlg setNameFieldStringValue:@"untitled.txt"];
         NSInteger ret = [openDlg runModal];
         if (ret > 0) {
             NSString *path = [[openDlg URL] path];
-            return String.Create([path cStringUsingEncoding:NSUTF8StringEncoding]);
+            string_t result = String.Create([path cStringUsingEncoding:NSUTF8StringEncoding]);
+            
+            char *last_lf = strrchr(result.c_str,'\n');
+            if (last_lf != NULL && last_lf[1] == '\0') {
+                last_lf[0] = '\0';
+            }
+            return result;
         } else {
             return String.Create(NULL);
         }
